cf30cda24aabe15eb4824aca3f95782495248ad7
rexx-gif <coderedem1500k@gmail.com>
2026-07-25 18:15:19 +0700

    implement custom authentication login page and layout for Filament

---
+++ app/Filament/Pages/Auth/CustomLogin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Auth\Pages\Login as BaseLogin;
+
+class CustomLogin extends BaseLogin
+{
+    protected string $view = 'filament.pages.auth.login';
+    protected static string $layout = 'filament.pages.auth.layout';
+}
